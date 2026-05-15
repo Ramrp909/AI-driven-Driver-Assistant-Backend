0fba32757304af35dad0bd458a1c3ef84664344d
Ramrp909 <ramprasadpanthagiri90@gmail.com>
2026-05-15 17:03:58 +0530

    ai monitoring actions add

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ async def detect_face(file: UploadFile = File(...)):
 
     head_direction = "Center"
     looking_away = False
+    attention_score = 100
 
     global drowsy_counter
     global looking_away_counter
@@ -86,6 +87,9 @@ async def detect_face(file: UploadFile = File(...)):
         minNeighbors=5,
         minSize=(30, 30)
     )
+    face_detected = len(faces) > 0
+    if not face_detected:
+        attention_status = "No Face"
 
     # MediaPipe eye tracking
     if results.multi_face_landmarks:
@@ -123,6 +127,9 @@ async def detect_face(file: UploadFile = File(...)):
 
             # Confirm only after few frames
             looking_away = looking_away_counter > 3
+            if looking_away:
+                attention_status = "Distracted"
+
             # Left eye landmarks
             left_eye_top = face_landmarks.landmark[159]
             left_eye_bottom = face_landmarks.landmark[145]
@@ -134,7 +141,13 @@ async def detect_face(file: UploadFile = File(...)):
             )
 
             # Threshold for closed eye
-            # if eye_distance < 0.015:
+
+            
+            if eye_distance < 0.015 and not looking_away:
+                    drowsy_counter += 1
+            else:
+                     drowsy_counter = 0
+            
             if eye_distance < 0.015 and not looking_away:
                 if drowsy_counter > 4:
                     is_drowsy = True
@@ -143,20 +156,30 @@ async def detect_face(file: UploadFile = File(...)):
             else:
                     is_drowsy = False
                     attention_status = "Focused"
-                
 
+            if attention_status == "Focused":
+                attention_score = 96
 
-            if eye_distance < 0.015 and not looking_away:
-                    drowsy_counter += 1
+            elif attention_status == "Distracted":
+                attention_score = 65
+
+            elif attention_status == "Drowsy":
+                attention_score = 30
+
             else:
-                     drowsy_counter = 0
+                attention_score = 0
+
+
 
     return {
-        "faceDetected": len(faces) > 0,
+        "faceDetected": face_detected,
+        
         "faceCount": len(faces),
         "isDrowsy": is_drowsy,
         "attentionStatus": attention_status,
 
         "headDirection": head_direction,
         "lookingAway": looking_away,
+
+        "attentionScore": attention_score,
     }
